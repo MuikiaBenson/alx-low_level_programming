dd888b60e2560ed22b7eadf1bce9b6df8ad89b09
benny <benmuigai20@gmail.com>
2023-10-08 18:20:28 +0300

    quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main- this is the netry pointmain functio)
+ * Return: 1 on sucess
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
