681d27cd20d27f26255cc0457b4a1cc93fc439b7
benny <benmuigai20@gmail.com>
2023-10-11 19:14:54 +0300

    omit q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main- this is the main function
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		if (alp != 'q' && alp != 'e')
+		{
+			putchar(alp);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
