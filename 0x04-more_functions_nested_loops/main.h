f6822a9ac1783230e57ad7fa6667abf5c2ee72af
benny <benmuigai20@gmail.com>
2023-10-13 02:43:46 +0300

    print numbers 0..9

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
 int mul(int a, int b);
 int _isdigit(int c);
 int _isupper(int c);
@@ -8,3 +12,6 @@ void print_numbers(void);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+void print_number(int n);
+
+#endif
